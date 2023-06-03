f86b4acbe4596fa7e7e47ad048dc7a241a116306
aiswaryajayadev <92874003+aiswaryajayadev@users.noreply.github.com>
2023-06-03 21:50:42 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<style>
+.mydiv {
+       
+	  
+}
+
+
+
+</style>
+<div  class="mydiv" > 
+<div class="header">
+     <div class="container">
+       
+            <h1 style="color : white"><b><u>College Amenity Booking System</h1></b></u>
+      
+         <div class="top-menu">
+		 
+         <ul>  
+         <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+          <li> <a href="about.php"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>About</a></li>
+          <li>  <a href="services.php"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>Services</a></li>
+		  <li><a href="user/login.php"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>Login</a></li>           
+           <li><a href="admin/login.php"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>Admin</a></li>
+		   <li><a href="contact.php"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>Contact</a></li>
+           </ul> 
+         </div>
+         <div class="clearfix"></div>
+     </div>
+</div>
+</div>
